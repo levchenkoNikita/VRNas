5f865c69dc575c99a8d20c765bf4e9bc3056769d
Levchenko Nikita <avantix5@gmail.com>
2025-10-30 10:59:53 +0300

    Продолжаю работать надо шапкой. Сделал бургер кнопку и сейчас нахожусь на этапе создания оверлэя

--- scripts/Header.js
+++ scripts/Header.js
@@ -3,7 +3,8 @@ class Header {
         menu: "[data-js-header-menu]",
         select: "[data-js-header-select]",
         selectButton: "[data-js-header-select-button]",
-        selectList: "[data-js-header-select-list]"
+        selectList: "[data-js-header-select-list]",
+        burgerButton: "[data-js-header-burger-button]"
     }
 
     stateClasses = {
@@ -12,16 +13,51 @@ class Header {
     }
 
     constructor() {
-        this.menuElement = document.querySelector(this.selectors.menu);
+        this.currentSelect = [];
         this.bindEvents();
     }
 
     onClick(event) {
-        console.log(event);
+        const clickElement = event.target;
+        
+        // Работа меню выбора Ссылок
+        if ("jsHeaderSelectButton" in clickElement.dataset) {
+            this.handleMenuList(clickElement);
+        }
+        else {
+            if (this.currentSelect.length > 0) {
+                this.currentSelect[1].classList.remove("is-active");
+                this.currentSelect[2].classList.remove("is-active");
+                this.currentSelect = [];
+            }
+        }
+
+        // Работа Бургер Кнопки
+        if("jsHeaderBurgerButton" in clickElement.dataset) {
+            this.handleBurgerButton(clickElement);
+        }
+    }
+
+    handleMenuList(clickElement) {
+        const selectElement = clickElement.closest(this.selectors.select);
+        const selectList = selectElement.querySelector(this.selectors.selectList);
+
+        if (this.currentSelect.length > 0 && this.currentSelect[0] != selectElement) {
+            this.currentSelect[1].classList.remove("is-active");
+            this.currentSelect[2].classList.remove("is-active");
+        }
+
+        clickElement.classList.toggle("is-active");
+        selectList.classList.toggle("is-active");
+        this.currentSelect = [selectElement, clickElement, selectList];
+    }
+
+    handleBurgerButton(clickElement) {
+        clickElement.classList.toggle("is-active");
     }
 
     bindEvents() {
-        this.menuElement.addEventListener('click', (event) => this.onClick(event));
+        document.addEventListener('click', (event) => this.onClick(event));
     }
 }
 
