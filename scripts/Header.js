b0a26293fbd0052d3e53130f2ff52fde1e204eac
Levchenko Nikita <avantix5@gmail.com>
2025-11-01 19:16:17 +0300

    Шапка и первый блок готовы!

--- scripts/Header.js
+++ scripts/Header.js
@@ -1,5 +1,6 @@
 class Header {
     selectors = {
+        header: "[data-js-header]",
         menu: "[data-js-header-menu]",
         select: "[data-js-header-select]",
         selectButton: "[data-js-header-select-button]",
@@ -9,10 +10,12 @@ class Header {
 
     stateClasses = {
         isActive: "is-active",
-        isLock: "is-lock"
+        isLock: "is-lock",
+        isScrolling: "is-scrolling"
     }
 
     constructor() {
+        this.headerElement = document.querySelector(this.selectors.header);
         this.menuElement = document.querySelector(this.selectors.menu);
         this.currentSelect = [];
         this.bindEvents();
@@ -27,8 +30,8 @@ class Header {
         }
         else {
             if (this.currentSelect.length > 0) {
-                this.currentSelect[1].classList.remove("is-active");
-                this.currentSelect[2].classList.remove("is-active");
+                this.currentSelect[1].classList.remove(this.stateClasses.isActive);
+                this.currentSelect[2].classList.remove(this.stateClasses.isActive);
                 this.currentSelect = [];
             }
         }
@@ -39,27 +42,46 @@ class Header {
         }
     }
 
+    onScroll() {
+        const isScrolling = window.scrollY > 0;
+
+        if(isScrolling) {
+            this.headerElement.classList.add(this.stateClasses.isScrolling);
+        }
+        else {
+            this.headerElement.classList.remove(this.stateClasses.isScrolling);
+        }
+    }
+
     handleMenuList(clickElement) {
         const selectElement = clickElement.closest(this.selectors.select);
         const selectList = selectElement.querySelector(this.selectors.selectList);
 
         if (this.currentSelect.length > 0 && this.currentSelect[0] != selectElement) {
-            this.currentSelect[1].classList.remove("is-active");
-            this.currentSelect[2].classList.remove("is-active");
+            this.currentSelect[1].classList.remove(this.stateClasses.isActive);
+            this.currentSelect[2].classList.remove(this.stateClasses.isActive);
         }
 
-        clickElement.classList.toggle("is-active");
-        selectList.classList.toggle("is-active");
+        clickElement.classList.toggle(this.stateClasses.isActive);
+        selectList.classList.toggle(this.stateClasses.isActive);
         this.currentSelect = [selectElement, clickElement, selectList];
     }
 
     handleBurgerButton(clickElement) {
-        clickElement.classList.toggle("is-active");
-        this.menuElement.classList.toggle("is-active");
+        const isScrolling = this.headerElement.classList.contains(this.stateClasses.isScrolling);
+
+        if(!isScrolling) {
+            this.headerElement.classList.toggle(this.stateClasses.isActive);
+        }
+
+        clickElement.classList.toggle(this.stateClasses.isActive);
+        this.menuElement.classList.toggle(this.stateClasses.isActive);
+        document.documentElement.classList.toggle(this.stateClasses.isLock);
     }
 
     bindEvents() {
-        document.addEventListener('click', (event) => this.onClick(event));
+        document.documentElement.addEventListener('click', (event) => this.onClick(event));
+        window.addEventListener('scroll', () => this.onScroll());
     }
 }
 
