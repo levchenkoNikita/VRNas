ce9c9414bf747550f0d44d0cd0429154638b0572
Vitaliy Mitrofanov <mitrofanov2122@gmail.com>
2025-11-12 19:00:13 +0300

    добавлен stylint и eslint

--- scripts/About.js
+++ scripts/About.js
@@ -1,43 +1,43 @@
 class About {
-	selectors = {
-		aboutAnimation: '[data-js-about-animation]',
-		aboutAnimationLeft: '[data-js-about-animation-left]'
-	}
-	
-	stateClasses = {
-		isVisible: 'is-visible',
-		isVisibleForLeft: 'is-visible-for-left'
-	}
-	
-	constructor() {
-		this.aboutAnimationElements = document.querySelectorAll(this.selectors.aboutAnimation)
-		this.aboutAnimationLeftElements = document.querySelectorAll(this.selectors.aboutAnimationLeft)
-		this.initScrollAnimation()
-	}
-	
-	initScrollAnimation() {
-		if (this.aboutAnimationElements.length === 0 &&
-			this.aboutAnimationLeftElements.length === 0) return
-		
-		const observer = new IntersectionObserver(entries => {
-			entries.forEach(entry => {
-				if(entry.target.hasAttribute('data-js-about-animation-left') && entry.isIntersecting) {
-					entry.target.classList.add(this.stateClasses.isVisibleForLeft )
-				}
-				if (entry.isIntersecting) {
-					entry.target.classList.add(this.stateClasses.isVisible)
-				}
-			})
-		})
-		
-		this.aboutAnimationElements.forEach(element => {
-			observer.observe(element)
-		})
-		this.aboutAnimationLeftElements.forEach(element => {
-			observer.observe(element)
-		})
-	}
+  selectors = {
+    aboutAnimation: '[data-js-about-animation]',
+    aboutAnimationLeft: '[data-js-about-animation-left]'
+  };
+
+  stateClasses = {
+    isVisible: 'is-visible',
+    isVisibleForLeft: 'is-visible-for-left'
+  };
+
+  constructor() {
+    this.aboutAnimationElements = document.querySelectorAll(this.selectors.aboutAnimation);
+    this.aboutAnimationLeftElements = document.querySelectorAll(this.selectors.aboutAnimationLeft);
+    this.initScrollAnimation();
+  }
+
+  initScrollAnimation() {
+    if (this.aboutAnimationElements.length === 0 &&
+      this.aboutAnimationLeftElements.length === 0) return;
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.target.hasAttribute('data-js-about-animation-left') && entry.isIntersecting) {
+          entry.target.classList.add(this.stateClasses.isVisibleForLeft);
+        }
+        if (entry.isIntersecting) {
+          entry.target.classList.add(this.stateClasses.isVisible);
+        }
+      });
+    });
+
+    this.aboutAnimationElements.forEach(element => {
+      observer.observe(element);
+    });
+    this.aboutAnimationLeftElements.forEach(element => {
+      observer.observe(element);
+    });
+  }
 }
 
 
-export default About
+export default About;
--- scripts/Header.js
+++ scripts/Header.js
@@ -1,88 +1,86 @@
 class Header {
-    selectors = {
-        header: "[data-js-header]",
-        menu: "[data-js-header-menu]",
-        select: "[data-js-header-select]",
-        selectButton: "[data-js-header-select-button]",
-        selectList: "[data-js-header-select-list]",
-        burgerButton: "[data-js-header-burger-button]"
-    }
+  selectors = {
+    header: "[data-js-header]",
+    menu: "[data-js-header-menu]",
+    select: "[data-js-header-select]",
+    selectButton: "[data-js-header-select-button]",
+    selectList: "[data-js-header-select-list]",
+    burgerButton: "[data-js-header-burger-button]"
+  };
 
-    stateClasses = {
-        isActive: "is-active",
-        isLock: "is-lock",
-        isScrolling: "is-scrolling"
-    }
+  stateClasses = {
+    isActive: "is-active",
+    isLock: "is-lock",
+    isScrolling: "is-scrolling"
+  };
 
-    constructor() {
-        this.headerElement = document.querySelector(this.selectors.header);
-        this.menuElement = document.querySelector(this.selectors.menu);
+  constructor() {
+    this.headerElement = document.querySelector(this.selectors.header);
+    this.menuElement = document.querySelector(this.selectors.menu);
+    this.currentSelect = [];
+    this.bindEvents();
+  }
+
+  onClick(event) {
+    const clickElement = event.target;
+
+    // Работа меню выбора Ссылок
+    if ("jsHeaderSelectButton" in clickElement.dataset) {
+      this.handleMenuList(clickElement);
+    } else {
+      if (this.currentSelect.length > 0) {
+        this.currentSelect[1].classList.remove(this.stateClasses.isActive);
+        this.currentSelect[2].classList.remove(this.stateClasses.isActive);
         this.currentSelect = [];
-        this.bindEvents();
+      }
     }
 
-    onClick(event) {
-        const clickElement = event.target;
-        
-        // Работа меню выбора Ссылок
-        if ("jsHeaderSelectButton" in clickElement.dataset) {
-            this.handleMenuList(clickElement);
-        }
-        else {
-            if (this.currentSelect.length > 0) {
-                this.currentSelect[1].classList.remove(this.stateClasses.isActive);
-                this.currentSelect[2].classList.remove(this.stateClasses.isActive);
-                this.currentSelect = [];
-            }
-        }
+    // Работа Бургер Кнопки
+    if ("jsHeaderBurgerButton" in clickElement.dataset) {
+      this.handleBurgerButton(clickElement);
+    }
+  }
 
-        // Работа Бургер Кнопки
-        if("jsHeaderBurgerButton" in clickElement.dataset) {
-            this.handleBurgerButton(clickElement);
-        }
+  onScroll() {
+    const isScrolling = window.scrollY > 0;
+
+    if (isScrolling) {
+      this.headerElement.classList.add(this.stateClasses.isScrolling);
+    } else {
+      this.headerElement.classList.remove(this.stateClasses.isScrolling);
+    }
+  }
+
+  handleMenuList(clickElement) {
+    const selectElement = clickElement.closest(this.selectors.select);
+    const selectList = selectElement.querySelector(this.selectors.selectList);
+
+    if (this.currentSelect.length > 0 && this.currentSelect[0] != selectElement) {
+      this.currentSelect[1].classList.remove(this.stateClasses.isActive);
+      this.currentSelect[2].classList.remove(this.stateClasses.isActive);
     }
 
-    onScroll() {
-        const isScrolling = window.scrollY > 0;
+    clickElement.classList.toggle(this.stateClasses.isActive);
+    selectList.classList.toggle(this.stateClasses.isActive);
+    this.currentSelect = [selectElement, clickElement, selectList];
+  }
 
-        if(isScrolling) {
-            this.headerElement.classList.add(this.stateClasses.isScrolling);
-        }
-        else {
-            this.headerElement.classList.remove(this.stateClasses.isScrolling);
-        }
+  handleBurgerButton(clickElement) {
+    const isScrolling = this.headerElement.classList.contains(this.stateClasses.isScrolling);
+
+    if (!isScrolling) {
+      this.headerElement.classList.toggle(this.stateClasses.isActive);
     }
 
-    handleMenuList(clickElement) {
-        const selectElement = clickElement.closest(this.selectors.select);
-        const selectList = selectElement.querySelector(this.selectors.selectList);
+    clickElement.classList.toggle(this.stateClasses.isActive);
+    this.menuElement.classList.toggle(this.stateClasses.isActive);
+    document.documentElement.classList.toggle(this.stateClasses.isLock);
+  }
 
-        if (this.currentSelect.length > 0 && this.currentSelect[0] != selectElement) {
-            this.currentSelect[1].classList.remove(this.stateClasses.isActive);
-            this.currentSelect[2].classList.remove(this.stateClasses.isActive);
-        }
-
-        clickElement.classList.toggle(this.stateClasses.isActive);
-        selectList.classList.toggle(this.stateClasses.isActive);
-        this.currentSelect = [selectElement, clickElement, selectList];
-    }
-
-    handleBurgerButton(clickElement) {
-        const isScrolling = this.headerElement.classList.contains(this.stateClasses.isScrolling);
-
-        if(!isScrolling) {
-            this.headerElement.classList.toggle(this.stateClasses.isActive);
-        }
-
-        clickElement.classList.toggle(this.stateClasses.isActive);
-        this.menuElement.classList.toggle(this.stateClasses.isActive);
-        document.documentElement.classList.toggle(this.stateClasses.isLock);
-    }
-
-    bindEvents() {
-        document.documentElement.addEventListener('click', (event) => this.onClick(event));
-        window.addEventListener('scroll', () => this.onScroll());
-    }
+  bindEvents() {
+    document.documentElement.addEventListener('click', (event) => this.onClick(event));
+    window.addEventListener('scroll', () => this.onScroll());
+  }
 }
 
 export default Header;
